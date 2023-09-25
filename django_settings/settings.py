a9edc535130d1b4f94be145710e48bacedd9a957
AlmasZhambyl <almaszhambyl0098@gmail.com>
2023-09-25 14:37:03 +0600

    3-rd chance

--- django_settings/settings.py
+++ django_settings/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%cl#0_lppm0ys!)o!4*9hr*w069rvuyedfcq3+jb5e+j66@g^d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'django_settings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django_site/views.py
+++ django_site/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def site(request):
+    return None
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def site(request):
+    return render(request, 'site.html', {'data': "Python is Fun"})
